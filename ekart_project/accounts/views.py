916eb7fe5b9a8fde4443c9ddd8cf6f7b59084bee
Praveen Medla <pankajmendla@gmail.com>
2024-09-16 23:21:25 +0530

    created  login and logout  in  views in Accounts app

--- ekart_project/accounts/views.py
+++ ekart_project/accounts/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
-# Create your views here.
+from .models import Account
+from django.contrib import messages,auth
+from django.contrib.auth.decorators import login_required
+
+def login(request):
+    if request.method =="POST":
+        email = request.POST['email']
+        password = request.POST['password']
+
+        user = auth.authenticate(email=email,password=password)
+
+        if user is not None:
+            auth.login(request=user)
+
+        else:
+            messages.error(request,'invalid login credentials')
+            return redirect,'accounts/login.html'
+    return render(request,'Your are Logged out.')
+def logout(request):
+    auth.logout(request)
+    messages.success(request,'Your are Logged out.')
+    return redirect('login')
