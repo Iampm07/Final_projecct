097aea58e2e6e1f11644a94630148a697c76c9d7
Praveen Medla <pankajmendla@gmail.com>
2024-11-13 23:59:12 +0530

    updated store.html and order_complete.html

--- ekart_project/accounts/views.py
+++ ekart_project/accounts/views.py
@@ -3,11 +3,12 @@ from django.shortcuts import render,redirect
 from .forms import RegistrationForm
 from .models import Account
 from django.contrib import messages,auth
-
+import logging
+logger=logging.getLogger(__name__)
 
 def register(request):
     if request.method == 'POST':
-        print("request.POST=",request.POST)
+        
         form = RegistrationForm(request.POST)
         if form.is_valid():
             first_name = form.cleaned_data['first_name']
@@ -16,22 +17,25 @@ def register(request):
             email = form.cleaned_data['email'] # pankajmendla@gmail.com
             password = form.cleaned_data['password']
             username = email.split("@")[0]
-            user = Account.objects.create_user(first_name=first_name, last_name=last_name, email=email, username=username, password=password)
+            user = Account.objects.create_user(first_name=first_name,
+                last_name=last_name, 
+                email=email,
+                username=username,
+                password=password
+                )
             user.phone_number = phone_number
             user.save()
         
             return redirect('login')
         else:
+            logger.warning("form is invalid:%s",form.errors)
+    else:
          form = RegistrationForm()
          context = {
          'form': form,
            }
-        return render(request, 'accounts/register.html', context)
-    else:
-         form = RegistrationForm()
-
-    return render(request, 'accounts/register.html', {'form': form})
-
+    return render(request, 'accounts/register.html', context)
+    
 
 
 
@@ -44,8 +48,8 @@ def register(request):
 
 def login(request):
     if request.method == 'POST':
-        email = request.POST['email']
-        password = request.POST['password']
+        email = request.POST.get('email')
+        password = request.POST.get('password')
 
         user = auth.authenticate(email=email, password=password)
         print("user=",user)
